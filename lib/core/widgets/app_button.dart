9f456d791c720c06fb6ece597bcc1c8f2724466e
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-31 18:12:10 +0300

    -added app button to widgets folder

---
+++ lib/core/widgets/app_button.dart
@@ -0,0 +1,50 @@
+import 'package:car_rental_app/core/utils/extension.dart';
+import 'package:car_rental_app/core/utils/hex_color.dart';
+import 'package:car_rental_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class AppButton extends StatelessWidget {
+  final double? width;
+  final double? height;
+  final Color? color;
+  final BorderRadiusGeometry? borderRadius;
+  final String? title;
+  final TextStyle? style;
+  final AlignmentGeometry? alignment;
+  final TextAlign? textAlign;
+  final Function()? onTap;
+
+  const AppButton({
+    super.key,
+    this.width,
+    this.height,
+    this.color,
+    this.borderRadius,
+    this.title,
+    this.style,
+    this.alignment,
+    this.textAlign,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width ?? double.infinity,
+        height: height ?? context.screenHeight / 15.5,
+        alignment: alignment ?? Alignment.center,
+        decoration: BoxDecoration(
+          color: color ?? HexColor(hexColor: '21292B'),
+          borderRadius: borderRadius ?? BorderRadius.circular(62),
+        ),
+        child: Text(
+          textAlign: textAlign ?? TextAlign.center,
+          title ?? "Get Started",
+          style: style ?? TextStyles.font18WhiteBold,
+        ),
+      ),
+    );
+  }
+}
