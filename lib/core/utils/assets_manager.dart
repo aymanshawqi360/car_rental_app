ccd005a774c3483bd3cf72fd2b5fb521c2b85746
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-31 18:07:00 +0300

    -added assets manager to utils folder

---
+++ lib/core/utils/assets_manager.dart
@@ -0,0 +1,10 @@
+class AssetsManager {
+  static const String _images = "assets/images/";
+  static const String _svg = "assets/svg/";
+
+  //!Images
+  static const String onboardingPageOne = "${_images}onboarding_page_one.png";
+  static const String onboardingPageTwo = "${_images}onboarding_page_two.jpg";
+  //!Svg
+  static const String cartIcon = "${_svg}car_icon.svg";
+}
