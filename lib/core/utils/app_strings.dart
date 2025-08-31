be9232ae32512bd3d9d701563acde7f5d514aeb4
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-31 18:06:29 +0300

    -added app strings to utils folder

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,8 @@
+class AppStrings {
+  AppStrings._();
+  static const String onboardingTitle =
+      "Lets Start\nA New ExperienceWithCar rental.";
+  static const String roboto = "Roboto";
+  static const String description =
+      "Discover your next adventure with Qent. we’re here to provide you with a seamless car rental experience.Let’s get started on your journey.";
+}
