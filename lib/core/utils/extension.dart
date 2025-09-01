acd0a4b787541983eea405886627888a847ed39a
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-01 22:13:03 +0300

    -added amendment on extension folder

--- lib/core/utils/extension.dart
+++ lib/core/utils/extension.dart
@@ -26,10 +26,14 @@ extension Navigation on BuildContext {
 
 extension ContextExtensions on BuildContext {
   Size get screenSize => MediaQuery.sizeOf(this);
+  MediaQueryData get paddingSize => MediaQuery.of(this);
+  AppBar get appBar => AppBar();
 
   double get screenWidth => screenSize.width;
   double get screenHeight => screenSize.height;
-
+  double get appBarHeight => appBar.preferredSize.height;
+  double get bodyHeight =>
+      screenHeight - paddingSize.padding.top - appBarHeight;
   EdgeInsets get customPadding => EdgeInsets.only(
     left: screenWidth / 13,
     top: screenHeight / 50,
