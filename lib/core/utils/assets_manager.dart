c8e06653e218b64936c9c7ee825fa255c3745a2d
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-01 22:11:32 +0300

    -added images and svg new to asstes manager

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -7,4 +7,9 @@ class AssetsManager {
   static const String onboardingPageTwo = "${_images}onboarding_page_two.jpg";
   //!Svg
   static const String cartIcon = "${_svg}car_icon.svg";
+  static const String qent = "${_svg}qent.svg";
+  static const String qentCar = "${_svg}qent_car.svg";
+    static const String checkBox = "${_svg}check_box.svg";
+  static const String googleIcon = "${_svg}google_icon.svg";
+  static const String appleIcon = "${_svg}apple_icon.svg";
 }
