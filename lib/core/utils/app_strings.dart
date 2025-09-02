81f9f496aec67734aa32e6f435018611a11c2202
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-02 23:08:17 +0300

    -added amendment to app strings

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -8,10 +8,13 @@ class AppStrings {
   static const String qent = "Qent";
   static const String rememberMe = "Remember Me";
   static const String forgotPassword = "Forgot Password";
-  static const String welcomeBackReayToHitTheRoad="Welcome Back\nReady to hit the road.";
-  static const String signUp="Sign Up.";
-  static const String login="Login.";
-  static const String dontHaveAnAccountSignUp="Don't have an account?  ";
-  static const String dontHaveAnAccountLogin="Don't have an account? ";
-  static const String password="Password";
+  static const String welcomeBackReayToHitTheRoad =
+      "Welcome Back\nReady to hit the road.";
+  static const String signUp = "Sign Up";
+  static const String login = "Login";
+  static const String dontHaveAnAccountSignUp = "Don't have an account?  ";
+  static const String dontHaveAnAccountLogin = "Already have an account? ";
+  static const String password = "Password";
+  static const String googlePay = "Google Pay";
+  static const String applePay = "Apple Pay";
 }
