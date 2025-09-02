da66eac67285c21d3637f6f1a0dbd9d2a3fe1ad6
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-02 22:44:31 +0300

    -added dont have an account to widgets folder

---
+++ lib/core/widgets/dont_have_an_account.dart
@@ -0,0 +1,40 @@
+import 'package:car_rental_app/core/routing/routes.dart';
+import 'package:car_rental_app/core/utils/extension.dart';
+import 'package:car_rental_app/core/utils/styles.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class DontHaveAnAccount extends StatelessWidget {
+  final String fristText;
+  final String lateText;
+  const DontHaveAnAccount({
+    super.key,
+    required this.fristText,
+    required this.lateText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  context.pushNamedAndRemoveUntil(
+                    Routes.login,
+                    predicate: (_) => false,
+                  );
+                },
+              style: TextStyles.font16OnyxRegular,
+              text: lateText,
+            ),
+          ],
+          style: TextStyles.font16GrayRegular,
+          text: fristText,
+        ),
+      ),
+    );
+  }
+}
