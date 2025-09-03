b406dfb4fad3b776878a6a33aac5edf33929e7ee
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-03 04:43:16 +0300

    -added appbar widget in widgtes

---
+++ lib/core/widgets/appbar_widget.dart
@@ -0,0 +1,32 @@
+import 'package:car_rental_app/core/utils/assets_manager.dart';
+import 'package:car_rental_app/core/utils/extension.dart';
+import 'package:car_rental_app/core/utils/spacing.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class AppbarWidget extends StatelessWidget {
+  final double? height;
+  const AppbarWidget({super.key, this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height ?? context.bodyHeight * 0.15,
+      child: Row(
+        children: [
+          SvgPicture.asset(
+            AssetsManager.qentCar,
+            width: context.screenWidth * 0.036,
+            height: context.screenHeight * 0.036,
+          ),
+          horizontalSpacing(context.screenWidth / 60),
+          SvgPicture.asset(
+            AssetsManager.qent,
+            width: context.screenWidth * 0.022,
+            height: context.screenHeight * 0.022,
+          ),
+        ],
+      ),
+    );
+  }
+}
