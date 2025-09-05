20ef7d5014c770a180431b44871dee7b3c1edc96
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-05 03:21:49 +0300

    -added verification code field to widget folder

---
+++ lib/features/auth/verification_code/presentation/widget/verification_code_field.dart
@@ -0,0 +1,49 @@
+import 'package:car_rental_app/core/utils/app_colors.dart';
+import 'package:car_rental_app/core/utils/extension.dart';
+import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+class VerificationCodeField extends StatelessWidget {
+  const VerificationCodeField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PinCodeTextField(
+      length: 4,
+      keyboardType: TextInputType.number,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      obscureText: false,
+      animationType: AnimationType.fade,
+      cursorColor: ColorsManager.black,
+      cursorHeight: context.screenHeight * 0.015,
+      pinTheme: PinTheme(
+        shape: PinCodeFieldShape.box,
+        borderRadius: BorderRadius.circular(13),
+        fieldHeight: context.screenHeight / 11.7,
+        fieldWidth: context.screenWidth / 5.5,
+        activeFillColor: ColorsManager.white,
+        activeColor: ColorsManager.lightGray,
+        inactiveColor: ColorsManager.lightGray,
+        inactiveFillColor: ColorsManager.white,
+        selectedColor: ColorsManager.lightGray,
+        selectedFillColor: ColorsManager.white,
+        // errorBorderColor: Colors.red,
+        // disabledColor: Colors.amber,
+      ),
+      animationDuration: Duration(milliseconds: 300),
+      backgroundColor: Colors.transparent,
+      enableActiveFill: true,
+      onCompleted: (v) {
+        print("Completed");
+      },
+      onChanged: (value) {
+        print(value);
+      },
+      beforeTextPaste: (text) {
+        print("Allowing to paste $text");
+        return false;
+      },
+      appContext: context,
+    );
+  }
+}
