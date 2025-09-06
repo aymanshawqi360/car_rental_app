fa5e26b4ff60f73350919b02af93a6accf456b83
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-06 16:38:26 +0300

    -added a change app appbar file

--- lib/core/widgets/app_appbar.dart
+++ lib/core/widgets/app_appbar.dart
@@ -2,6 +2,7 @@ import 'package:car_rental_app/core/utils/assets_manager.dart';
 import 'package:car_rental_app/core/utils/extension.dart';
 import 'package:car_rental_app/core/utils/spacing.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
 class AppAppbar extends StatelessWidget implements PreferredSizeWidget {
@@ -10,9 +11,12 @@ class AppAppbar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      surfaceTintColor: Theme.of(context).scaffoldBackgroundColor,
+      elevation: 0.0,
       title: Row(
         children: [
-          SvgPicture.asset(AssetsManager.qentCar, width: 33, height: 33),
+          SvgPicture.asset(AssetsManager.qentCar, width: 33.w, height: 33.h),
           horizontalSpacing(context.screenWidth * 0.02),
           SvgPicture.asset(AssetsManager.qent, width: 20, height: 20),
         ],
