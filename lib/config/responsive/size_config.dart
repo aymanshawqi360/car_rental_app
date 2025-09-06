e535c8861d0a7d4b426fe5448dd7c16d19466c8b
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-06 16:32:59 +0300

    -added size config to responsive folder

---
+++ lib/config/responsive/size_config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SizeConfig {
+  SizeConfig._();
+  static double heightButton = 43.h;
+  static double heightButtonSocialMedia = 35.h;
+}
