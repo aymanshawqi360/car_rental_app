057651667a31b4017a68c12be8ad0dfa4e755eb5
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-06 16:42:26 +0300

    -added a change of login screen file

--- lib/features/auth/login/presentation/page/login_screen.dart
+++ lib/features/auth/login/presentation/page/login_screen.dart
@@ -2,9 +2,9 @@ import 'package:car_rental_app/core/utils/app_strings.dart';
 import 'package:car_rental_app/core/utils/extension.dart';
 import 'package:car_rental_app/core/utils/spacing.dart';
 import 'package:car_rental_app/core/utils/styles.dart';
+import 'package:car_rental_app/core/widgets/app_appbar.dart';
 import 'package:car_rental_app/core/widgets/app_text_form_field.dart';
 import 'package:car_rental_app/core/widgets/app_divider_and_or.dart';
-import 'package:car_rental_app/core/widgets/appbar_widget.dart';
 import 'package:car_rental_app/features/auth/login/presentation/widget/login_and_sing_up_button_login.dart';
 import 'package:car_rental_app/features/auth/login/presentation/widget/remember_me_and_forgot_password.dart';
 import 'package:car_rental_app/features/auth/login/presentation/widget/rich_text.dart';
@@ -17,41 +17,50 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppAppbar(),
       body: Padding(
         padding: EdgeInsetsGeometry.only(
           left: context.screenWidth * 0.03,
           right: context.screenWidth * 0.03,
-          // top: context.bodyHeight * 0.02,
-          // bottom: context.bodyHeight * 0.02,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
 
           children: [
-            AppbarWidget(),
+            verticalSpacing(context.bodyHeight * 0.06),
             SizedBox(
-              height: context.bodyHeight * 0.13,
+              height: context.bodyHeight * 0.14,
               child: Text(
                 AppStrings.welcomeBackReayToHitTheRoad,
 
                 style: TextStyles.font24BlackSemiBold,
               ),
             ),
+
             SizedBox(
-              child: Column(
-                children: [
-                  AppTextFormField(),
-                  verticalSpacing(context.bodyHeight * 0.02),
-                  AppTextFormField(hintText: AppStrings.password),
-                ],
-              ),
+              height: context.bodyHeight * 0.1,
+              child: AppTextFormField(),
             ),
-            RememberMeAndForgotPassword(),
-            LoginAndSingUpButtonLogin(),
-            AppDividerAndOr(),
-            verticalSpacing(context.bodyHeight * 0.02),
-            SocialMediaButton(),
-            verticalSpacing(context.bodyHeight * 0.1),
+
+            AppTextFormField(hintText: AppStrings.password),
+            SizedBox(
+              height: context.bodyHeight * 0.1,
+              child: RememberMeAndForgotPassword(),
+            ),
+            SizedBox(
+              height: context.bodyHeight * 0.19,
+              child: LoginAndSingUpButtonLogin(),
+            ),
+            SizedBox(
+              height: context.bodyHeight * 0.07,
+              child: AppDividerAndOr(),
+            ),
+
+            SizedBox(
+              height: context.bodyHeight * 0.2,
+
+              child: Column(children: [SocialMediaButton()]),
+            ),
             RichTextWidget(),
           ],
         ),
