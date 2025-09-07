87f311f8413520f7e3d53804562e698474f9501d
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-07 16:54:31 +0300

    -added a page name to routes file

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,6 +1,7 @@
 class Routes {
   Routes._();
-  static const String onboarding = "/";
+  static const String splash = "/";
+  static const String onboarding = "/onboarding";
   static const String login = "/login";
   static const String signUp = "/signUp";
   static const String resetPassword = "/resetPassword";
