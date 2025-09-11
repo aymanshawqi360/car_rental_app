657d8195abc09e74307bff1cb836a254a764e5f7
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-12 00:19:44 +0300

    -added a change in api_sign_up_service

--- lib/features/auth/sign_up/data/api/api_sign_up_service.dart
+++ lib/features/auth/sign_up/data/api/api_sign_up_service.dart
@@ -1,6 +1,6 @@
 import 'package:car_rental_app/core/network/dio_factory.dart';
 import 'package:car_rental_app/features/auth/sign_up/data/api/api_sign_up_constants.dart';
-import 'package:car_rental_app/features/auth/sign_up/data/models/singup_request_body.dart';
+import 'package:car_rental_app/features/auth/sign_up/data/models/sign_up_request_body.dart';
 import 'package:dio/dio.dart';
 
 class ApiSignUpService {
@@ -8,7 +8,7 @@ class ApiSignUpService {
 
   ApiSignUpService({required this.dioFactory});
 
-  Future<Response> singUp({required SingUpRequestBody requestApiModel}) async {
+  Future<Response> singUp({required SignUpRequestBody requestApiModel}) async {
     try {
       final response = await dioFactory.post(
         ApiSingUpConstants.singUpPath,
