be2304b1c91d589ab21231993d6659476383eaf9
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-12 00:20:11 +0300

    -added a change in sign_up_repo_implementation

--- lib/features/auth/sign_up/data/repo_impl/sign_up_repo_implementation.dart
+++ lib/features/auth/sign_up/data/repo_impl/sign_up_repo_implementation.dart
@@ -5,7 +5,7 @@ import 'package:car_rental_app/features/auth/sign_up/data/models/sign_up_request
 import 'package:car_rental_app/features/auth/sign_up/data/models/sign_up_response.dart';
 import 'package:car_rental_app/features/auth/sign_up/domain/repo/sign_up_repo_domain.dart';
 
-class SignUpRepoImplementation extends SingUpRepoDomain {
+class SignUpRepoImplementation extends SignUpRepoDomain {
   final ApiSignUpService apiService;
 
   SignUpRepoImplementation({required this.apiService});
