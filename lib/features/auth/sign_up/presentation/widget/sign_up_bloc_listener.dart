283e8f6475bec9b366283f7bb64d9b6954e8d59b
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-13 14:57:50 +0300

    -added a change to sign_up_bloc_listener

--- lib/features/auth/sign_up/presentation/widget/sign_up_bloc_listener.dart
+++ lib/features/auth/sign_up/presentation/widget/sign_up_bloc_listener.dart
@@ -1,17 +1,16 @@
-import 'package:car_rental_app/core/utils/app_colors.dart';
+import 'package:car_rental_app/core/error/show_error_message.dart';
 import 'package:car_rental_app/core/utils/extension.dart';
-import 'package:car_rental_app/features/auth/sign_up/presentation/cubit/singup_cubit.dart';
-import 'package:car_rental_app/features/auth/sign_up/presentation/cubit/singup_state.dart';
+import 'package:car_rental_app/features/auth/sign_up/presentation/cubit/sign_up_cubit.dart';
+import 'package:car_rental_app/features/auth/sign_up/presentation/cubit/sign_up_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignUpBlocListener extends StatelessWidget {
   const SignUpBlocListener({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<SingUpCubit, SingUpState>(
+    return BlocListener<SignUpCubit, SingUpState>(
       listenWhen: (previous, current) =>
           current is SingupLoading ||
           current is SingupSuccess ||
@@ -37,31 +36,8 @@ class SignUpBlocListener extends StatelessWidget {
 
 void _setupErrorState(BuildContext context, SingupFailure state) {
   // context.pop();
-  showDialog(
+  ShowErrorMessage.instance.showErrorMessage(
     context: context,
-
-    builder: (context) {
-      return AlertDialog(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        // icon: const Icon(Icons.error, color: Colors.red, size: 32),
-        title: Text(
-          "Error",
-          style: TextStyle(fontSize: 15.sp, color: Colors.red),
-        ),
-
-        content: Text(
-          state.apiErrorModel.allSignUpError(),
-          // style: TextStyles.font14DarkTaupeRegular,
-        ),
-        actions: [
-          GestureDetector(
-            onTap: () {
-              context.pop();
-            },
-            child: Text("Cancel", style: TextStyle(color: ColorsManager.onyx)),
-          ),
-        ],
-      );
-    },
+    errorMessage: state.apiErrorModel.allSignUpError(),
   );
 }
