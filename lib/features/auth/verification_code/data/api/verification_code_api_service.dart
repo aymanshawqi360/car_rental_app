14e5d54b9d950dc3abcc9c513da8315cf863afc2
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-27 20:01:47 +0300

    -added a change to verification_code_api_service verification_code api

--- lib/features/auth/verification_code/data/api/verification_code_api_service.dart
+++ lib/features/auth/verification_code/data/api/verification_code_api_service.dart
@@ -1,6 +1,7 @@
 import 'package:car_rental_app/core/network/dio_factory.dart';
 import 'package:car_rental_app/features/auth/verification_code/data/api/verification_constants.dart';
-import 'package:car_rental_app/features/auth/verification_code/data/model/verification_code_request_body.dart';
+import 'package:car_rental_app/features/auth/verification_code/data/model/confirm_verify_code_request.dart';
+import 'package:car_rental_app/features/auth/verification_code/data/model/request_verify_code.dart';
 import 'package:dio/dio.dart';
 
 class VerificationCodeApiService {
@@ -9,11 +10,25 @@ class VerificationCodeApiService {
   VerificationCodeApiService({required this.dioFactory});
 
   Future<Response> checkIfPhoneNumber({
-    required VerificationCodeRequestBody request,
+    required RequestVerifyCode request,
   }) async {
     try {
       final response = await dioFactory.post(
         VerificationConstants.requestVerifyCode,
+        body: request.toJson(request),
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Response> confirmVerifyCode({
+    required ConfirmVerifyCodeRequest request,
+  }) async {
+    try {
+      final response = await dioFactory.post(
+        VerificationConstants.confirmVerifyCode,
         body: request.toJson(request),
       );
       return response;
