3a7717db45f73694a8cb411dfd7bfe01f29904ce
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-27 20:03:59 +0300

    -added a change to verification_code_repo_implementation verification_code repo_impl

--- lib/features/auth/verification_code/data/repo_impl/verification_code_repo_implementation.dart
+++ lib/features/auth/verification_code/data/repo_impl/verification_code_repo_implementation.dart
@@ -1,7 +1,10 @@
+import 'package:car_rental_app/config/Shared/user_data_response.dart';
 import 'package:car_rental_app/core/error/api_error_handler.dart';
 import 'package:car_rental_app/core/error/api_resulte.dart';
 import 'package:car_rental_app/features/auth/verification_code/data/api/verification_code_api_service.dart';
-import 'package:car_rental_app/features/auth/verification_code/data/model/verification_code_request_body.dart';
+import 'package:car_rental_app/features/auth/verification_code/data/model/confirm_verify_code_request.dart';
+import 'package:car_rental_app/features/auth/verification_code/data/model/verification_code_api_response.dart';
+import 'package:car_rental_app/features/auth/verification_code/data/model/request_verify_code.dart';
 import 'package:car_rental_app/features/auth/verification_code/domain/repo/verification_code_repo.dart';
 
 class VerificationCodeRepoImplementation extends VerificationCodeRepo {
@@ -11,14 +14,31 @@ class VerificationCodeRepoImplementation extends VerificationCodeRepo {
     required this.verificationCodeApiService,
   });
   @override
-  Future<ApiResulte<dynamic>> checkIfPhoneNumber(
-    VerificationCodeRequestBody request,
+  Future<ApiResulte<VerificationCodeApiResponse>> checkIfPhoneNumber(
+    RequestVerifyCode request,
   ) async {
     try {
       final response = await verificationCodeApiService.checkIfPhoneNumber(
         request: request,
       );
-      return ApiResulte.success(response);
+
+      return ApiResulte.success(
+        VerificationCodeApiResponse.fromJson(response.data),
+      );
+    } catch (e) {
+      return ApiResulte.failure(ApiErrorHandler.apiHandler(e));
+    }
+  }
+
+  @override
+  Future<ApiResulte<UserDataResponse>> confirmVerifyCode(
+    ConfirmVerifyCodeRequest request,
+  ) async {
+    try {
+      final response = await verificationCodeApiService.confirmVerifyCode(
+        request: request,
+      );
+      return ApiResulte.success(UserDataResponse.fromJson(response.data));
     } catch (e) {
       return ApiResulte.failure(ApiErrorHandler.apiHandler(e));
     }
