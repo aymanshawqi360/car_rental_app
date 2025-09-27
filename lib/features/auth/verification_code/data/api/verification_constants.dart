34a6193a28109e96c3c3f39a02c2a31a9628a3c5
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-27 20:02:22 +0300

    -added a change to verification_constants verification_code api

--- lib/features/auth/verification_code/data/api/verification_constants.dart
+++ lib/features/auth/verification_code/data/api/verification_constants.dart
@@ -1,4 +1,5 @@
 class VerificationConstants {
   VerificationConstants._();
   static const String requestVerifyCode = "/auth/phone/request_verify_code/";
+  static const String confirmVerifyCode = "/auth/phone/confirm_verify_code/";
 }
