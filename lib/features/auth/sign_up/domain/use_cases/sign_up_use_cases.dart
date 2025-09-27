29634eb77a9bae3f5b27adc26d68a6fc6663de2b
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-27 18:09:05 +0300

    -added a change to sign_up_use_cases sign_up use_cases

--- lib/features/auth/sign_up/domain/use_cases/sign_up_use_cases.dart
+++ lib/features/auth/sign_up/domain/use_cases/sign_up_use_cases.dart
@@ -1,6 +1,8 @@
 import 'package:car_rental_app/core/error/api_resulte.dart';
 import 'package:car_rental_app/features/auth/sign_up/data/models/sign_up_request_body.dart';
 import 'package:car_rental_app/features/auth/sign_up/data/models/sign_up_response.dart';
+import 'package:car_rental_app/features/auth/sign_up/domain/entity/sign_up_countries_entity.dart';
+import 'package:car_rental_app/features/auth/sign_up/domain/entity/sign_up_location_entity.dart';
 import 'package:car_rental_app/features/auth/sign_up/domain/repo/sign_up_repo_domain.dart';
 
 class SignUpUseCases {
@@ -12,5 +14,17 @@ class SignUpUseCases {
     required SignUpRequestBody requestApiModel,
   }) {
     return singUpRepoDomain.singUp(requestApiModel);
+  }
+
+  Future<ApiResulte<List<SignUpCountriesEntity>>> getCountires({
+    required int query,
+  }) {
+    return singUpRepoDomain.getCountries(query: query);
+  }
+
+  Future<ApiResulte<List<SignUpLocationEntity>>> getLocation({
+    required int query,
+  }) {
+    return singUpRepoDomain.getLocation(query: query);
   }
 }
