181b208ea72ee66f78b96c44d0763aa4fdd7d1b8
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-27 20:06:07 +0300

    -added a change to verification_code_use_cases verification_code use_cases

--- lib/features/auth/verification_code/domain/use_cases/verification_code_use_cases.dart
+++ lib/features/auth/verification_code/domain/use_cases/verification_code_use_cases.dart
@@ -1,4 +1,8 @@
-import 'package:car_rental_app/features/auth/verification_code/data/model/verification_code_request_body.dart';
+import 'package:car_rental_app/config/Shared/user_data_response.dart';
+import 'package:car_rental_app/core/error/api_resulte.dart';
+import 'package:car_rental_app/features/auth/verification_code/data/model/confirm_verify_code_request.dart';
+import 'package:car_rental_app/features/auth/verification_code/data/model/verification_code_api_response.dart';
+import 'package:car_rental_app/features/auth/verification_code/data/model/request_verify_code.dart';
 import 'package:car_rental_app/features/auth/verification_code/domain/repo/verification_code_repo.dart';
 
 class VerificationCodeUseCases {
@@ -6,10 +10,17 @@ class VerificationCodeUseCases {
 
   VerificationCodeUseCases({required this.verificationCodeRepo});
 
-  Future<dynamic> checkIfPhoneNumber({
-    required VerificationCodeRequestBody request,
+  Future<ApiResulte<VerificationCodeApiResponse>> checkIfPhoneNumber({
+    required RequestVerifyCode request,
   }) {
     final respose = verificationCodeRepo.checkIfPhoneNumber(request);
+    return respose;
+  }
+
+  Future<ApiResulte<UserDataResponse>> confirmVerifyCode({
+    required ConfirmVerifyCodeRequest request,
+  }) {
+    final respose = verificationCodeRepo.confirmVerifyCode(request);
     return respose;
   }
 }
