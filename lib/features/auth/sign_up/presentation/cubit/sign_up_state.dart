2afd4704795db02f05518caa3527ae6d0c7ae30c
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-27 19:57:30 +0300

    -added a change to sign_up_state sign_up cibit

--- lib/features/auth/sign_up/presentation/cubit/sign_up_state.dart
+++ lib/features/auth/sign_up/presentation/cubit/sign_up_state.dart
@@ -1,37 +1,43 @@
 import 'package:car_rental_app/core/error/api_error_model.dart';
+import 'package:car_rental_app/features/auth/sign_up/domain/entity/sign_up_countries_entity.dart';
+import 'package:car_rental_app/features/auth/sign_up/domain/entity/sign_up_location_entity.dart';
 import 'package:equatable/equatable.dart';
 
-sealed class SingUpState extends Equatable {}
+sealed class SignUpState extends Equatable {}
 
-class SingupInitial extends SingUpState {
+class SignUpInitial extends SignUpState {
   @override
   List<Object?> get props => [];
 }
 
-class SingupLoading extends SingupInitial {
+class SingUpLoading extends SignUpInitial {
   @override
   List<Object?> get props => [];
 }
 
-class SingupSuccess extends SingupInitial {
+class SingUpSuccess extends SignUpInitial {
+  final String accessToken;
+  final String phone;
+
+  SingUpSuccess({required this.accessToken, required this.phone});
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [accessToken, phone];
 }
 
-class SingupFailure extends SingupInitial {
+class SingUpFailure extends SignUpInitial {
   final ApiErrorModel apiErrorModel;
 
-  SingupFailure({required this.apiErrorModel});
+  SingUpFailure({required this.apiErrorModel});
   @override
   List<Object?> get props => [apiErrorModel];
 }
 
-class SingUpString extends SingupInitial {
+class AddValuesCountry extends SignUpInitial {
   final String countryCode;
   final String countryName;
   final int maxLength;
 
-  SingUpString({
+  AddValuesCountry({
     required this.countryCode,
     required this.countryName,
     required this.maxLength,
@@ -40,7 +46,77 @@ class SingUpString extends SingupInitial {
   List<Object?> get props => [countryCode, countryName, maxLength];
 }
 
-class ValidateUser extends SingupInitial {
+class AddValuesLocation extends SignUpInitial {
+  final String locationName;
+  final double lat;
+  final double lng;
+
+  AddValuesLocation({
+    required this.locationName,
+    required this.lat,
+    required this.lng,
+  });
+  @override
+  List<Object?> get props => [locationName, lat, lng];
+}
+
+class ValidateUser extends SignUpInitial {
   @override
   List<Object?> get props => [];
+}
+
+class SingUpCountryLoading extends SignUpInitial {
+  @override
+  List<Object?> get props => [];
+}
+
+class SingUpCountrySuccess extends SignUpInitial {
+  final List<SignUpCountriesEntity> countries;
+
+  SingUpCountrySuccess({required this.countries});
+  @override
+  List<Object?> get props => [countries];
+}
+
+class SingUpCountryFailure extends SignUpInitial {
+  final ApiErrorModel errorMessage;
+
+  SingUpCountryFailure({required this.errorMessage});
+  @override
+  List<Object?> get props => [errorMessage];
+}
+
+class SignUpLocationLoading extends SignUpInitial {}
+
+class SignUpLocationSuccess extends SignUpInitial {
+  final List<SignUpLocationEntity> countryList;
+
+  SignUpLocationSuccess({required this.countryList});
+  @override
+  List<Object?> get props => [countryList];
+}
+
+class SignUpLocationFailure extends SignUpInitial {
+  final ApiErrorModel errorMessage;
+
+  SignUpLocationFailure({required this.errorMessage});
+
+  @override
+  List<Object?> get props => [errorMessage];
+}
+
+class SignUpAvailableToCreateCar extends SignUpInitial {
+  final bool value;
+
+  SignUpAvailableToCreateCar({required this.value});
+  @override
+  List<Object?> get props => [value];
+}
+
+class SignUpObscureText extends SignUpInitial {
+  final bool value;
+
+  SignUpObscureText({required this.value});
+  @override
+  List<Object?> get props => [value];
 }
