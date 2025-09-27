847194843c6176a5cc0727000d9fa6e120d4334f
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-27 20:04:58 +0300

    -added a change to verification_code_repo verification_code repo_impl

--- lib/features/auth/verification_code/domain/repo/verification_code_repo.dart
+++ lib/features/auth/verification_code/domain/repo/verification_code_repo.dart
@@ -1,8 +1,14 @@
+import 'package:car_rental_app/config/Shared/user_data_response.dart';
 import 'package:car_rental_app/core/error/api_resulte.dart';
-import 'package:car_rental_app/features/auth/verification_code/data/model/verification_code_request_body.dart';
+import 'package:car_rental_app/features/auth/verification_code/data/model/confirm_verify_code_request.dart';
+import 'package:car_rental_app/features/auth/verification_code/data/model/verification_code_api_response.dart';
+import 'package:car_rental_app/features/auth/verification_code/data/model/request_verify_code.dart';
 
 abstract class VerificationCodeRepo {
-  Future<ApiResulte<void>> checkIfPhoneNumber(
-    VerificationCodeRequestBody request,
+  Future<ApiResulte<VerificationCodeApiResponse>> checkIfPhoneNumber(
+    RequestVerifyCode request,
+  );
+  Future<ApiResulte<UserDataResponse>> confirmVerifyCode(
+    ConfirmVerifyCodeRequest request,
   );
 }
