d2364840744355f12480a07e49865519f5732d5f
aymanshawqi360 <aymanshawqi380gmail.com>
2025-09-27 20:08:52 +0300

    -added a change to verification_code_state verification_code cubit

--- lib/features/auth/verification_code/presentation/cubit/verification_code_state.dart
+++ lib/features/auth/verification_code/presentation/cubit/verification_code_state.dart
@@ -10,14 +10,54 @@ sealed class VerificationCodeState extends Equatable {
 
 class VerificationCodeInitial extends VerificationCodeState {}
 
-class VerificationCodeLoading extends VerificationCodeState {}
+class CheckIfPhoneNumberLoading extends VerificationCodeState {}
 
-class VerificationCodeSuccess extends VerificationCodeState {}
+class CheckIfPhoneNumberSuccess extends VerificationCodeState {
+  final String message;
+  final String code;
+  final String verifyToken;
+  final String phone;
 
-class VerificationCodeFailure extends VerificationCodeState {
+  const CheckIfPhoneNumberSuccess({
+    required this.message,
+    required this.code,
+    required this.verifyToken,
+    required this.phone,
+  });
+  @override
+  List<Object> get props => [message, code, verifyToken, phone];
+}
+
+class CheckIfPhoneNumberFailure extends VerificationCodeState {
   final ApiErrorModel errorMessage;
 
-  const VerificationCodeFailure({required this.errorMessage});
+  const CheckIfPhoneNumberFailure({required this.errorMessage});
   @override
   List<Object> get props => [errorMessage];
+}
+
+class ConfirmVerifyCodeFailure extends VerificationCodeState {
+  final ApiErrorModel errorMessage;
+
+  const ConfirmVerifyCodeFailure({required this.errorMessage});
+  @override
+  List<Object> get props => [errorMessage];
+}
+
+class ConfirmVerifyCodeLoading extends VerificationCodeState {
+  @override
+  List<Object> get props => [];
+}
+
+class ConfirmVerifyCodeSuccess extends VerificationCodeState {
+  @override
+  List<Object> get props => [];
+}
+
+class ConfirmVerifyCodePhone extends VerificationCodeState {
+  final String phone;
+
+  const ConfirmVerifyCodePhone({required this.phone});
+  @override
+  List<Object> get props => [phone];
 }
