d24b83bd5bedaa6a848a0d0a57e15e9ecb3840b7
aymanshawqi360 <aymanshawqi380gmail.com>
2025-10-05 23:21:03 +0300

    - added brand_bloc_listener to brands folder - added brand_item to brands folder - added brand_loading_shimmer to brandes folder

---
+++ lib/features/home/presentation/widget/brands/brand_item.dart
@@ -0,0 +1,44 @@
+import 'package:car_rental_app/core/utils/colors_manager.dart';
+import 'package:car_rental_app/core/utils/styles.dart';
+import 'package:car_rental_app/core/widgets/app_cahed_network_image.dart';
+import 'package:car_rental_app/features/home/domain/entity/brand_entity.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BrandItem extends StatelessWidget {
+  final BrandEntity brandEntity;
+  const BrandItem({super.key, required this.brandEntity});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        ClipOval(
+          child: AppCahedNetworkImage(
+            image: brandEntity.image ?? "",
+            width: 53.w,
+            height: 53.h,
+            fit: BoxFit.contain,
+            placeholder: CircleAvatar(
+              radius: 25.r,
+              backgroundColor: ColorsManager.gray,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 70.w,
+          child: Text(
+            brandEntity.name.toString(),
+            textAlign: TextAlign.center,
+            style: TextStyles.font12GrayRegular.copyWith(
+              fontWeight: FontWeight.w600,
+            ),
+            // softWrap: true,
+            // overflow: TextOverflow.visible,
+          ),
+        ),
+      ],
+    );
+  }
+}
