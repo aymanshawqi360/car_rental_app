c43f3183cc173046e3850669bb059c1ece9e13f4
aymanshawqi360 <aymanshawqi380gmail.com>
2025-10-05 23:12:44 +0300

    -added home_repo to repo folder

---
+++ lib/features/home/domain/repo/home_repo.dart
@@ -0,0 +1,6 @@
+import 'package:car_rental_app/core/error/api_resulte.dart';
+import 'package:car_rental_app/features/home/domain/entity/brand_entity.dart';
+
+abstract class HomeRepo {
+  Future<ApiResulte<List<BrandEntity>>> getBrands();
+}
