a12ffb051d07c908b982d8094395a5599356b017
aymanshawqi360 <aymanshawqi380gmail.com>
2025-10-05 23:13:18 +0300

    -added brands_user_case to user_cases folder

---
+++ lib/features/home/domain/user_cases/brands_user_case.dart
@@ -0,0 +1,13 @@
+import 'package:car_rental_app/core/error/api_resulte.dart';
+import 'package:car_rental_app/features/home/domain/entity/brand_entity.dart';
+import 'package:car_rental_app/features/home/domain/repo/home_repo.dart';
+
+class BrandsUserCase {
+  final HomeRepo homeRepo;
+
+  BrandsUserCase({required this.homeRepo});
+
+  Future<ApiResulte<List<BrandEntity>>> getBrands() {
+    return homeRepo.getBrands();
+  }
+}
